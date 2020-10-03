30f241af972ef5300e53ba7cba7a9ec83a0f3ec0
Mert <mertdemirok80@gmail.com>
2020-10-03 15:48:59 +0200

    Data Types

---
+++ Chapter05/src/Exercise_05_01/DataTypesApp.java
@@ -0,0 +1,26 @@
+package Exercise_05_01;
+
+public class DataTypesApp {
+    public static void main(String[] args) {
+
+        boolean aBoolean = false;
+        char aCharacter = 'd';
+        byte aByte = 126;
+        short aShort = 1568;
+        int anInteger = 0b0101_1100;
+        long aLong = 45631341L;
+        float aDecimalNumber = 1256.32F;
+        double aBigDecimal = 12.365987451236;
+        final double THE_VALUE_OF_PI = 3.14;
+
+
+        System.out.println(aBoolean);
+        System.out.println(aCharacter);
+        System.out.println(aByte);
+        System.out.println(aShort);
+        System.out.println(anInteger);
+        System.out.println(aLong);
+        System.out.println(aDecimalNumber);
+        System.out.println(aBigDecimal);
+    }
+}
