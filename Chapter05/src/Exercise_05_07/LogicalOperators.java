ce7f3baa599f251e84f2bb4b7249b5e4900884e3
Mert <mertdemirok80@gmail.com>
2020-10-03 23:08:13 +0200

    logical operators

---
+++ Chapter05/src/Exercise_05_07/LogicalOperators.java
@@ -0,0 +1,15 @@
+package Exercise_05_07;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b= false;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("a && b: " + (a && b));
+        System.out.println("a || b: " + (a || b));
+        System.out.println("!a: " + !a);
+        System.out.println("!b: " + !b);
+    }
+}
