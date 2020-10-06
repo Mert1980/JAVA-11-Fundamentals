499ff956c0ddbf9ae017c2341ff0e016dcc60c3d
Mert1980 <mertdemirok80@gmail.com>
2020-10-06 11:49:34 +0200

    For loop, program one

---
+++ Chapter05/src/Exercise_05_17/ForLoopOne.java
@@ -0,0 +1,9 @@
+package Exercise_05_17;
+
+public class ForLoopOne {
+    public static void main(String[] args) {
+        for(int i = 400; i >= 350; i--){
+            System.out.println(i);
+        }
+    }
+}
