55a96f94647c7b4082d336dc65c71e1d00240777
Mert1980 <mertdemirok80@gmail.com>
2020-10-06 11:55:27 +0200

    Find the multiples of 7 smaller than 200.

---
+++ Chapter05/src/Exercise_05_17/ForLoopTwo.java
@@ -0,0 +1,11 @@
+package Exercise_05_17;
+
+public class ForLoopTwo {
+    public static void main(String[] args) {
+        for (int i = 1; i < 200 ; i++) {
+           if(i % 7 == 0){
+               System.out.println(i);
+           }
+        }
+    }
+}
