019d466fd4642b2d22ad1f73cd7db7c22ad5e34f
Mert <mertdemirok80@gmail.com>
2020-10-07 00:23:16 +0200

    Calculate body mass index by applying classes and methods

--- Chapter05/src/Exercise_05_18/BmiApplication.java
+++ Chapter05/src/Exercise_05_18/BmiApplication.java
@@ -1,7 +1,5 @@
 package Exercise_05_18;
 
-import java.util.Scanner;
-
 public class BmiApplication {
     public static void main(String[] args) {
         System.out.println("Enter your length (cm):");
@@ -9,8 +7,8 @@ public class BmiApplication {
         System.out.println("Enter your weight (kg):");
         int weight = KeyboardUtility.readInt();
 
-        float bmi = BmiUtility.calculateBmi(weight, height);
-
+        double bmi = BmiUtility.calculatePmi(weight, height);
+        BmiUtility.printDiagnose(bmi);
     }
 }
 
--- Chapter05/src/Exercise_05_18/BmiUtility.java
+++ Chapter05/src/Exercise_05_18/BmiUtility.java
@@ -1,8 +1,12 @@
 package Exercise_05_18;
 
 public class BmiUtility {
-    public static float calculatePmi(int weight, int height){
-        float pmi = (float) weight / height * height;
-        return pmi;
+    public static double calculatePmi(int weight, int height){
+        double pmi = (double) weight / ((double)height/100 * height/100);
+        return Math.round(pmi * 10.0)/10.0;
+    }
+
+    public static void printDiagnose(double bmi){
+        System.out.println("Your body mass index: " + bmi);
     }
 }
