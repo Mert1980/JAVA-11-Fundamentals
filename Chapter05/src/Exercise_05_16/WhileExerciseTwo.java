0af792d49e162f35aea139427fbb8835dc63deb1
Mert1980 <mertdemirok80@gmail.com>
2020-10-06 12:28:54 +0200

    refactor code

--- Chapter05/src/Exercise_05_16/WhileExerciseTwo.java
+++ Chapter05/src/Exercise_05_16/WhileExerciseTwo.java
@@ -2,16 +2,11 @@ package Exercise_05_16;
 
 public class WhileExerciseTwo {
     public static void main(String[] args) {
-        int number = 50;
+        int number = 3;
 
-        while(true){
-            if(number <= 0){
-                break;
-            }
-            number--;
-            if(number % 3 == 0){
-                System.out.println(number);
-            }
+        while(number < 50){
+            System.out.println(number);
+            number +=3;
         }
     }
 }
