7481a70c596aee1ee7eaafbc0575bf61e5019a4d
Mert1980 <mertdemirok80@gmail.com>
2020-10-06 09:27:50 +0200

    Assignment Operators

---
+++ Chapter05/src/Exercise_05_10/AssignmentOperator.java
@@ -0,0 +1,14 @@
+package Exercise_05_10;
+
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+        int number1 = 57;
+        int number2 = 33;
+        System.out.println(number1 += number2);
+        System.out.println(number1 -= number2);
+        System.out.println(number1 *= number2);
+        System.out.println(number1 /= number2);
+        System.out.println(number1 %= number2);
+    }
+}
