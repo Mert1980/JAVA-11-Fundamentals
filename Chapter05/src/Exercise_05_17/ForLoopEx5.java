c9d2d7b9bfcfb04da3cb0f0b0a756188e193bcf3
Mert1980 <mertdemirok80@gmail.com>
2020-10-06 14:02:51 +0200

    write numbers between a defined range, but positive numbers with plus sign

---
+++ Chapter05/src/Exercise_05_17/ForLoopEx5.java
@@ -0,0 +1,9 @@
+package Exercise_05_17;
+
+public class ForLoopEx5 {
+    public static void main(String[] args) {
+        for (int i = -10; i <= 10 ; i++) {
+            System.out.println(i > 0 ? ("+" + i) : i);
+        }
+    }
+}
