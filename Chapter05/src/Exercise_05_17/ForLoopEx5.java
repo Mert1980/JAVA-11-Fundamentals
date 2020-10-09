da1985fb9596fe1e84d836654706495af0bc5f14
Mert1980 <mertdemirok80@gmail.com>
2020-10-09 10:45:46 +0200

    apply shorter way

--- Chapter05/src/Exercise_05_17/ForLoopEx5.java
+++ Chapter05/src/Exercise_05_17/ForLoopEx5.java
@@ -4,11 +4,9 @@ public class ForLoopEx5 {
     public static void main(String[] args) {
         for (int i = -10; i <= 10 ; i++) {
             if(i > 0){
-                System.out.println("+" + i);
-            } else {
-                System.out.println(i);
+                System.out.print("+");
             }
-
+                System.out.println(i);
         }
     }
 }
