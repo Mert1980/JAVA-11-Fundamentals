efaa9153ab0500fd270304122695c6af6e28fd6f
Mert1980 <mertdemirok80@gmail.com>
2020-10-09 10:35:43 +0200

    apply shorter way

--- Chapter05/src/Exercise_05_17/ForLoopTwo.java
+++ Chapter05/src/Exercise_05_17/ForLoopTwo.java
@@ -2,10 +2,8 @@ package Exercise_05_17;
 
 public class ForLoopTwo {
     public static void main(String[] args) {
-        for (int i = 1; i < 200 ; i++) {
-           if(i % 7 == 0){
-               System.out.println(i);
-           }
+        for (int i = 7; i < 200 ; i+=7) {
+          System.out.println(i);
         }
     }
 }
