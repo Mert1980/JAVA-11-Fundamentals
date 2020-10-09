a5c049f63a80b786a56243e0bf1ba0e404677c15
Mert1980 <mertdemirok80@gmail.com>
2020-10-09 10:46:57 +0200

    apply indentation

--- Chapter05/src/Exercise_05_17/ForLoopEx5.java
+++ Chapter05/src/Exercise_05_17/ForLoopEx5.java
@@ -2,11 +2,11 @@ package Exercise_05_17;
 
 public class ForLoopEx5 {
     public static void main(String[] args) {
-        for (int i = -10; i <= 10 ; i++) {
-            if(i > 0){
+        for (int i = -10; i <= 10; i++) {
+            if (i > 0) {
                 System.out.print("+");
             }
-                System.out.println(i);
+            System.out.println(i);
         }
     }
 }
