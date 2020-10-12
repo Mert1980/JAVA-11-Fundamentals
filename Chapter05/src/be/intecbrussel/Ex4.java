31465e520033c56755ffb18cc9378267971ff430
Mert1980 <mertdemirok80@gmail.com>
2020-10-12 15:55:21 +0200

    correct the output

--- Chapter05/src/be/intecbrussel/Ex4.java
+++ Chapter05/src/be/intecbrussel/Ex4.java
@@ -42,7 +42,7 @@ public class Ex4 {
         if (digit1 >= 5) {
             numberOf5 = 1;
             if((9 - digit1) % 2 == 0){
-                numberOf2 = digit1 - 5;
+                numberOf2 = (digit1 - 5)/2;
             } else{
                 if (digit1 == 8){
                     numberOf2 = 1;
@@ -55,20 +55,44 @@ public class Ex4 {
             if(digit1 % 2 == 0){
                 numberOf2 = digit1 / 2;
             } else {
-                if (((5 - digit1) > 2)){
+                if (digit1 == 3){
                     numberOf2 = 1;
                     numberOf1 = 1;
                 } else {
-                    numberOf1 = 1;
+                    numberOf1 = digit1;
                 }
             }
         }
-        System.out.println("Number of 200 Euros needed: " + numberOf200);
-        System.out.println("Number of 100 Euros needed: " + numberOf100);
-        System.out.println("Number of 50 Euros needed: " + numberOf50);
-        System.out.println("Number of 10 Euros needed: " + numberOf10);
-        System.out.println("Number of 5 Euros needed: " + numberOf5);
-        System.out.println("Number of 2 Euros needed: " + numberOf2);
-        System.out.println("Number of 1 Euros needed: " + numberOf1);
+
+            if(amount == 500){
+                System.out.println("You need one 500 Euros banknote");
+            }
+
+            if (numberOf200 != 0){
+                System.out.println("Number of 200 Euros needed: " + numberOf200);
+            }
+            if (numberOf100 != 0){
+                System.out.println("Number of 100 Euros needed: " + numberOf100);
+            }
+
+            if (numberOf50 != 0){
+                System.out.println("Number of 50 Euros needed: " + numberOf50);
+            }
+
+            if (numberOf10 != 0){
+                System.out.println("Number of 10 Euros needed: " + numberOf10);
+            }
+
+            if (numberOf5 != 0){
+                System.out.println("Number of 5 Euros needed: " + numberOf5);
+            }
+
+            if (numberOf2 != 0){
+                System.out.println("Number of 2 Euros needed: " + numberOf2);
+            }
+
+            if (numberOf1 != 0){
+                System.out.println("Number of 1 Euros needed: " + numberOf1);
+            }
     }
 }
