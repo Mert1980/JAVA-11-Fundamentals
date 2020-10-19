42e4e21881da3ca95d3d3b02dbda9e83c88065b9
Mert1980 <mertdemirok80@gmail.com>
2020-10-19 14:29:56 +0200

    Convert the last 3 letters to uppercase in a given text

---
+++ Chapter05/src/ExtraExercises/LastThreeLetters.java
@@ -0,0 +1,18 @@
+package ExtraExercises;
+
+import java.util.Scanner;
+
+public class LastThreeLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a text:");
+        String text = scanner.nextLine();
+
+        int index = text.length() - 3;
+
+        String withoutLastThreeLetters = text.substring(0, index);
+        String lastThreeLetters = text.substring(index).toUpperCase();
+
+        System.out.println(withoutLastThreeLetters + lastThreeLetters);
+    }
+}
