6a8ac9354dffd28cd888cad8ef772d66d9f92549
Mert1980 <mertdemirok80@gmail.com>
2020-10-19 11:49:45 +0200

    print the sum of 2 given numbers unless the user wants to continue

--- Chapter05/src/ExtraExercises/Division.java
+++ Chapter05/src/ExtraExercises/Division.java
@@ -2,22 +2,22 @@ package ExtraExercises;
 
 public class Division {
     public static void main(String[] args) {
-        String divisableByThree = divideByThree();
-        System.out.println(divisableByThree);
+
+        System.out.println(divideByThree());
         System.out.println(divideByFive());
         System.out.println(divideByBoth());
     }
     public static String divideByThree(){
         String text = "";
         for (int i = 3; i < 100 ; i+=3) {
-            text = text +  i + " ";
+            text += i + " ";
         }
         return text;
     }
     public static String divideByFive(){
         String text = "";
         for (int i = 5; i < 100 ; i+=5) {
-            text = text +  i + " ";
+            text += i + " ";
         }
         return text;
     }
@@ -25,7 +25,7 @@ public class Division {
         String text = "";
         for (int i = 3; i < 100 ; i++) {
             if(i %3 == 0 && i %5 == 0){
-                text = text +  i + " ";
+                text += i + " ";
             }
         }
         return text;
