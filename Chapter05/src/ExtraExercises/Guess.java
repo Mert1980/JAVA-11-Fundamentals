ee970d63c8db2983c596f440cdb3cc2eafa276f1
Mert1980 <mertdemirok80@gmail.com>
2020-10-20 10:52:59 +0200

    optimize the code

--- Chapter05/src/ExtraExercises/Guess.java
+++ Chapter05/src/ExtraExercises/Guess.java
@@ -10,11 +10,11 @@ public class Guess {
 
         int randomNumber = rand.nextInt(10);
         System.out.println(randomNumber);
+        System.out.println("Guess the number:");
         int userInput;
         int numberOfTries = 0;
 
         do{
-            System.out.println("Guess the number:");
             userInput = scanner.nextInt();
             numberOfTries++;
             if(userInput < randomNumber){
@@ -23,15 +23,10 @@ public class Guess {
                 System.out.println("Try smaller ;)");
             }
         } while (userInput != randomNumber);
-
         printResult(numberOfTries);
     }
 
     public static void printResult(int counter){
-        if(counter == 1){
-            System.out.println("Congratulations! You found the number in " + counter + " attempt.");
-        } else {
-            System.out.println("Congratulations! You found the number in " + counter + " attempts.");
-        }
-     }
+        System.out.println("Congratulations! You found the number in " + counter + (counter ==1 ? " attempt." : " attempts"));
+    }
 }
