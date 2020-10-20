e48f5fbd6662182ede4e6131804a0e909c466127
Mert <mertdemirok80@gmail.com>
2020-10-20 21:06:41 +0200

    Write bubble sort algorithm

--- Chapter07_Arrays/src/Exercise07_01/BubbleSort.java
+++ Chapter07_Arrays/src/Exercise07_01/BubbleSort.java
@@ -5,38 +5,29 @@ import java.util.Random;
 public class BubbleSort {
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] numbers = new int[100];
-        int[] temp = new int[1];
 
+        int[] numbers = new int[50];
+        int[] temp = {0};
+        int counter = numbers.length;
 
-        for (int i = 0; i < 100 ; i++) {
-            numbers[i]= rand.nextInt(100);
-        }
-
-        for (int number:numbers
-        ) {
-            System.out.print(number + " ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(50);
+            System.out.print(numbers[i] + " ");
         }
 
         System.out.println("");
 
-        for (int i = 0; i < 99 ; i++) {
-            for (int j = 1; j < 100; j++) {
-                if (numbers[i] >= numbers[j]){
-                    temp[0] = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp[0];
-                } else {
-                    break;
+        do{
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i-1] >= numbers[i]){
+                    temp[0] = numbers[i-1];
+                    numbers[i-1] = numbers[i];
+                    numbers[i] = temp[0];
                 }
-            }
         }
-
-
-
-
-        for (int number:numbers
-             ) {
+            counter--;
+    } while(counter > 0);
+        for (int number:numbers) {
             System.out.print(number + " ");
         }
     }
