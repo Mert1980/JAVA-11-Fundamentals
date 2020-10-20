a568e34269eeab2911f6fd69f611226413d16654
Mert1980 <mertdemirok80@gmail.com>
2020-10-20 16:09:30 +0200

    find the prime numbers within a range using arrays

---
+++ Chapter07_Arrays/src/Exercise07_01/PrimeNumbers.java
@@ -0,0 +1,35 @@
+package Exercise07_01;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the limit: ");
+        int limit = scanner.nextInt();
+        int [] primeArray = new int[limit/2];
+
+        boolean isPrime = true;
+        int counter = 0;
+
+        for (int i = 1; i < limit ; i+=2) {
+            isPrime = true;
+
+           for (int j = 2; j < i/2; j++) {
+                if(i %j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                primeArray[counter] = i;
+                counter++;
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("");
+        for (int number: primeArray) {
+            System.out.print(number + " ");
+        }
+    }
+}
