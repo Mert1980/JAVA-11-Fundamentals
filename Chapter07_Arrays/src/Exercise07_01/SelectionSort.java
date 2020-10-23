736f3ad1d4436885800b3a8ceeb3b3eaddd51a98
Mert <mertdemirok80@gmail.com>
2020-10-23 06:03:00 +0200

    add comments

--- Chapter07_Arrays/src/Exercise07_01/SelectionSort.java
+++ Chapter07_Arrays/src/Exercise07_01/SelectionSort.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] listToBeSorted = { 0, -25, 6, 22, -64, 55, 23, 99, -1, 3 };
-        int[] newArray = listToBeSorted;
+        int[] newArray = listToBeSorted; // make a new array to be used as a right sub-array
 
         int index = 1;
         while(newArray.length > 1){
-            newArray = sort(newArray, 0);
-            int min = min(newArray);
-            listToBeSorted[index] = min;
+            newArray = sort(newArray, 0); // update the right sub-array by removing the min values
+            int min = min(newArray); // find the min value in the right sub-array
+            listToBeSorted[index] = min; // assign the found min value in the correct index number left to right
             index+=1;
         }
         for (int number:listToBeSorted
@@ -18,17 +18,19 @@ public class SelectionSort {
             System.out.print(number + " ");
         }
     }
-
+    // assign the min value in listToBeSorted in far most left index
+    // return the new right sub-array
+    // right sub-array will be shortened in every call of sort method by removing the min value in it
     public static int[] sort(int[] list, int index){
         int min = min(list);
         int swapIndex = findSwapMinIndex(list, min);
         list[swapIndex] = list[index];
         list[index] = min;
-
+    // make a new array excluding the indexes of previously found min numbers
         int[] newArray = Arrays.copyOfRange(list, index + 1, list.length );
         return newArray;
     }
-
+    // find the min number in an array
     public static int min(int... values){
         int min = values[0];
         for (int i = 0; i < values.length -1; i++) {
@@ -38,7 +40,7 @@ public class SelectionSort {
         }
         return min;
     }
-
+    // find the index number of the value that will be swapped with the index number of min value
     public static int findSwapMinIndex(int [] arr ,int min){
         if (arr == null) {
             return -1;
