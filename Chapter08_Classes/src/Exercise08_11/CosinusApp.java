2a281dbbc222d2e3f8fac28668d0915792575e4d
Mert1980 <mertdemirok80@gmail.com>
2020-10-27 11:09:44 +0100

    complete exercise 11

---
+++ Chapter08_Classes/src/Exercise08_11/CosinusApp.java
@@ -0,0 +1,19 @@
+package Exercise08_11;
+
+public class CosinusApp {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        System.out.println(Math.toDegrees(0.1));
+
+        for (double i = 0; i <= 2 * PI; i += 0.1) {
+            System.out.println(Math.cos(i) + " radiant");
+        }
+
+        System.out.println();
+        for (double i = 0; i <= 360 ; i += Math.toDegrees(0.1)) {
+            System.out.println(Math.cos(i) + " degrees");
+        }
+    }
+}
+
+
