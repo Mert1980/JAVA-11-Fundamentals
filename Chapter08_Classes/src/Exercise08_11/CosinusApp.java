52f0161059a8782364aab2aaeff9c111e602f0c8
Mert1980 <mertdemirok80@gmail.com>
2020-10-27 16:08:33 +0100

    refactor CosinusApp

--- Chapter08_Classes/src/Exercise08_11/CosinusApp.java
+++ Chapter08_Classes/src/Exercise08_11/CosinusApp.java
@@ -8,14 +8,14 @@ public class CosinusApp {
     public static void showCos(String unit){
         switch (unit){
             case "radiant":
-                calCosRad(Math.PI, 0.1, unit);
+                calCos(Math.PI*2, 0.1, unit);
             case "degrees":
-                calCosRad(180, Math.toDegrees(0.1), unit);
+                calCos(360, Math.toDegrees(0.1), unit);
         }
     }
 
-    public static void calCosRad(double limit, double period, String unit){
-        for (double i = 0; i <= 2 * limit; i += period) {
+    public static void calCos(double limit, double period, String unit){
+        for (double i = 0; i <= limit; i += period) {
             System.out.println(Math.cos(i) + " " + unit);
         }
     }
