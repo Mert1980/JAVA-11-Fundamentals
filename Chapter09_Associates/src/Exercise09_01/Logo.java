a8b1c70b75ad92505af2cad87394b06ef48ed758
Mert1980 <mertdemirok80@gmail.com>
2020-10-30 10:23:37 +0100

    change the main program so that it consists of array of rectangles and circles

--- Chapter09_Associates/src/Exercise09_01/Logo.java
+++ Chapter09_Associates/src/Exercise09_01/Logo.java
@@ -3,30 +3,49 @@ package Exercise09_01;
 import Exercise09_01_Aggregations.Circle;
 import Exercise09_01_Aggregations.Rectangle;
 
+import java.text.CharacterIterator;
+
 public class Logo {
-    private Rectangle rectangle;
-    private Circle circle;
+    private Rectangle[] rectangle = new Rectangle[5];
+    private Circle[] circle = new Circle[5];
     private String text;
 
     public Logo(Rectangle r, Circle c, String s){
-        this.rectangle = r;
-        this.circle = c;
+        this.rectangle[0] = r;
+        this.circle[0] = c;
         this.text = s;
     }
 
     public void setRectangle(Rectangle r){
-        this.rectangle = r;
+        int index = 0;
+        while(true){
+            if(this.rectangle[index] == null){
+                this.rectangle[index] = r;
+                break;
+            } else {
+                index++;
+            }
+        }
+
     }
 
-    public Rectangle getRectangle(){
+    public Rectangle[] getRectangle(){
         return this.rectangle;
     }
 
     public void setCircle(Circle c){
-        this.circle = c;
+        int index = 0;
+        while(true){
+            if(this.circle[index] != null){
+                this.circle[index] = c;
+                break;
+            } else {
+                index++;
+            }
+        }
     }
 
-    public Circle getCircle(){
+    public Circle[] getCircle(){
         return this.circle;
     }
 
@@ -45,8 +64,8 @@ public class Logo {
     @Override
     public String toString() {
         return "Logo{" +
-                "rectangle=" + rectangle + "\n"+
-                "     circle=" + circle + "\n"+
+                "rectangle=" + rectangle[0] + "\n" + rectangle[1] + "\n"+
+                "     circle=" + circle[0] + "\n"+
                 "     text='" + text + '\'' +
                 '}';
     }
--- Chapter09_Associates/src/Exercise09_01/LogoApp.java
+++ Chapter09_Associates/src/Exercise09_01/LogoApp.java
@@ -6,6 +6,8 @@ import Exercise09_01_Aggregations.Rectangle;
 public class LogoApp {
     public static void main(String[] args) {
         Logo myLogo = new Logo(new Rectangle(), new Circle(), "CodeWithMe");
+        myLogo.setRectangle(new Rectangle(2, 2, 2, 2));
         System.out.println(myLogo);
+
     }
 }
