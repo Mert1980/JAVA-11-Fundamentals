595a737e1ef9287bbb5394dd19237c896a2441dc
Mert1980 <mertdemirok80@gmail.com>
2020-10-30 12:34:31 +0100

    Create an array with 20 places and fill it with random cars

--- Chapter08_Classes/src/Exercise_20Oct2020/RandomCarApp.java
+++ Chapter08_Classes/src/Exercise_20Oct2020/RandomCarApp.java
@@ -13,19 +13,40 @@ public class RandomCarApp {
         System.out.println(secondCar);
         System.out.println(thirdCar);*/
 
-        Car firstCar = new Car(RandomGenerator.createRandomSpeed(), RandomGenerator.createRandomHorsePower(), RandomGenerator.createRandomColor(), RandomGenerator.createRandomLight());
+       /* Car firstCar = new Car(RandomGenerator.createRandomSpeed(), RandomGenerator.createRandomHorsePower(), RandomGenerator.createRandomColor(), RandomGenerator.createRandomLight());
         Car secondCar = new Car(RandomGenerator.createRandomSpeed(), RandomGenerator.createRandomHorsePower(), RandomGenerator.createRandomColor(), RandomGenerator.createRandomLight());
         Car thirdCar = new Car(RandomGenerator.createRandomSpeed(), RandomGenerator.createRandomHorsePower(), RandomGenerator.createRandomColor(), RandomGenerator.createRandomLight());
 
         System.out.println(firstCar);
         System.out.println(secondCar);
-        System.out.println(thirdCar);
+        System.out.println(thirdCar);*/
+
+       Car[] cars = new Car[20];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(RandomGenerator.createRandomSpeed(), RandomGenerator.createRandomHorsePower(), RandomGenerator.createRandomColor(), RandomGenerator.createRandomLight());
+        }
+
+        for (Car car:cars
+             ) {
+            System.out.println("Speed: " + car.getSpeed());
+        }
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i].parkCar();
+        }
+
+        for (Car car:cars
+        ) {
+            System.out.println("Parked cars: ");
+            if (car.getSpeed() == 0){
+                System.out.println(car);
+            }
+        }
     }
 
-    public static int getRandomNumber(int min, int max){
+    /*public static int getRandomNumber(int min, int max){
         Random rand = new Random();
         return rand.nextInt(max - min) + min;
-    }
-
-
+    }*/
 }
