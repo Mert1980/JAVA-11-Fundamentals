0882dd368adf093c3e6f0bb54283a90da7565f55
Mert1980 <mertdemirok80@gmail.com>
2020-10-30 10:56:33 +0100

    add accelerate, slowDown, repaint and toggleLight methods

--- Chapter08_Classes/src/Exercise_20Oct2020/Car.java
+++ Chapter08_Classes/src/Exercise_20Oct2020/Car.java
@@ -17,16 +17,16 @@ public class Car {
         this.speed = speed;
     }
 
-    public void setColor(String color) {
+    /*public void setColor(String color) {
         this.color = color;
-    }
+    }*/
 
     public void setHorsePower(int horsePower) {
         this.horsePower = horsePower;
     }
 
     public void setLightOn() {
-        this.isLightOn = !this.isLightOn;
+        this.isLightOn = true;
     }
 
     // getters
@@ -42,6 +42,38 @@ public class Car {
     public int getHorsePower() {
         return horsePower;
     }
+
+    // methods
+    public void accelerate(int speed){
+        this.speed+=speed;
+    }
+
+    public void slowDown(int speed){
+        if(this.speed > speed){
+            this.speed-=speed;
+        }
+    }
+
+    public void parkCar(){
+        this.speed = 0;
+        this.isLightOn = false;
+    }
+
+    public int rePaintCar(String color){
+        this.color = color;
+        int cost = 0;
+        char[] chars = color.toCharArray();
+        for (char letter:chars
+             ) {
+            cost+=letter;
+        }
+        return cost;
+    }
+
+    public void toggleLight(){
+        this.isLightOn = !this.isLightOn;
+    }
+
 
     @Override
     public String toString() {
--- Chapter08_Classes/src/Exercise_20Oct2020/CarApp.java
+++ Chapter08_Classes/src/Exercise_20Oct2020/CarApp.java
@@ -2,7 +2,7 @@ package Exercise_20Oct2020;
 
 public class CarApp {
     public static void main(String[] args) {
-        Car myCar = new Car();
+        /*Car myCar = new Car();
         System.out.println(myCar);
 
         Car my2ndCar = new Car();
@@ -12,7 +12,22 @@ public class CarApp {
 
         Car my3rdCar = new Car();
         my3rdCar.setHorsePower(200);
+        System.out.println(my3rdCar);*/
+
+        Car myCar = new Car();
+        System.out.println(myCar);
+        System.out.println(myCar.rePaintCar("red"));
+
+        Car my2ndCar = new Car();
+        my2ndCar.setSpeed(150);
+        my2ndCar.accelerate(30);
+        System.out.println(my2ndCar);
+
+        Car my3rdCar = new Car();
+        my3rdCar.setHorsePower(200);
+        my3rdCar.toggleLight();
         System.out.println(my3rdCar);
+
 
     }
 }
