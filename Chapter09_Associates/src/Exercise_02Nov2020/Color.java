aa33d9549e948f9b636a29cff23ffb7ed5edbb7e
Mert <mertdemirok80@gmail.com>
2020-11-02 10:58:49 +0100

    add constructor Color class

--- Chapter09_Associates/src/Exercise_02Nov2020/Color.java
+++ Chapter09_Associates/src/Exercise_02Nov2020/Color.java
@@ -1,4 +1,11 @@
 package Exercise_02Nov2020;
 
 public class Color {
+    private String name;
+    private int[] rgb;
+
+    public Color(String name, int[] rgb){
+        this.name = name;
+        this.rgb = rgb;
+    }
 }
