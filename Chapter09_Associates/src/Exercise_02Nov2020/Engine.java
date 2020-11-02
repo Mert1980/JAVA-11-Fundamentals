eab3fd1c247dba79cd25faba6bcc78fbf21e1ecb
Mert <mertdemirok80@gmail.com>
2020-11-02 10:45:11 +0100

    add constructor, burnFuel and reFuel methods in Engine class

--- Chapter09_Associates/src/Exercise_02Nov2020/Engine.java
+++ Chapter09_Associates/src/Exercise_02Nov2020/Engine.java
@@ -1,4 +1,17 @@
 package Exercise_02Nov2020;
 
 public class Engine {
+    private int fuel;
+
+    public Engine(int fuel){
+        this.fuel = fuel;
+    }
+
+    public void burnFuel(int amount){
+        this.fuel-=amount;
+    }
+
+    public void reFuel(int amount){
+        this.fuel+=amount;
+    }
 }
