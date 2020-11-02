6722740314abdb889194f79bad9e016d40771c5e
Mert <mertdemirok80@gmail.com>
2020-11-02 10:46:49 +0100

    add constructor Person class

--- Chapter09_Associates/src/Exercise_02Nov2020/Person.java
+++ Chapter09_Associates/src/Exercise_02Nov2020/Person.java
@@ -1,4 +1,11 @@
 package Exercise_02Nov2020;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
