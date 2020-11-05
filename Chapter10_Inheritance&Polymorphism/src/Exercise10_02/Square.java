ae156500bb64bde02d9d82d013ab4ed5d1a80352
Mert <mertdemirok80@gmail.com>
2020-11-05 14:30:38 +0100

    use inherited methods

---
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_02/Square.java
@@ -0,0 +1,5 @@
+package Exercise10_02;
+
+public class Square extends Rectangle{
+    public final String DESCRIPTION =  super.DESCRIPTION + "(Square)";
+}
---
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_02/SquareApp.java
@@ -0,0 +1,18 @@
+package Exercise10_02;
+
+public class SquareApp {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        System.out.println(sq.getArea()); // prints 1
+        System.out.println(sq.getPerimeter()); // prints 4
+
+        sq.setHeight(5);
+        sq.setWidth(5);
+        System.out.println(sq.getArea()); // prints 25
+        System.out.println(sq.getPerimeter()); // prints 20
+
+        sq.setPosition(3, 3);
+        System.out.println(sq.getX()); // prints 3
+        System.out.println(sq.getY()); // prints 3
+    }
+}
