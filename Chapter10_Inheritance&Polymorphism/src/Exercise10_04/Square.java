ec8b1a5c2ffba7527f0c201563be3d8b35d90d2b
Mert <mertdemirok80@gmail.com>
2020-11-05 15:17:34 +0100

    complete exercise 04 - method overriding

---
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_04/Square.java
@@ -0,0 +1,23 @@
+package Exercise10_04;
+
+public class Square extends Rectangle{
+    public final String DESCRIPTION =  super.DESCRIPTION + "(Square)";
+
+    public int getSide(){
+        return getHeight();
+    }
+
+    public void setSide(int side){
+        super.setHeight(side);
+        super.setWidth(side);
+    }
+    @Override
+    public void setHeight(int height){
+        setSide(height);
+    }
+
+    @Override
+    public void setWidth(int width){
+        setSide(width);
+    }
+}
---
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_04/SquareApp.java
@@ -0,0 +1,13 @@
+package Exercise10_04;
+
+public class SquareApp {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        sq.setHeight(5);
+        sq.setWidth(5);
+
+        System.out.println(sq.getArea()); // prints 25
+    }
+
+
+}
