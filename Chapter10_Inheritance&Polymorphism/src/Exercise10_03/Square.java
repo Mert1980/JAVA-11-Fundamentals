c20148eea33eb6b402171f26c2c3b67bf669a21a
Mert <mertdemirok80@gmail.com>
2020-11-05 14:57:20 +0100

    complete exercise 03 by adding methods to Square Class

---
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_03/Square.java
@@ -0,0 +1,14 @@
+package Exercise10_03;
+
+public class Square extends Rectangle {
+    public final String DESCRIPTION =  super.DESCRIPTION + "(Square)";
+
+    public int getSide(){
+        return getHeight();
+    }
+
+    public void setSide(int side){
+        setHeight(side);
+        setWidth(side);
+    }
+}
