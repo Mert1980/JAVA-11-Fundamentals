4b0bf84ab1550205cbde1f505ec7bdf400c1752d
Mert <mertdemirok80@gmail.com>
2020-11-06 05:42:22 +0100

    print Class methods of Rectangle

--- Chapter10_Inheritance&Polymorphism/src/Exercise10_05/SquareApp.java
+++ Chapter10_Inheritance&Polymorphism/src/Exercise10_05/SquareApp.java
@@ -2,10 +2,10 @@ package Exercise10_05;
 
 public class SquareApp {
     public static void main(String[] args) {
-        Square sq1 = new Square();
-        Square sq2 = new Square(5);
-        Square sq3 = new Square(7, 3, 3);
-        Square sq4 = new Square(new Square(8));
+        Rectangle sq1 = new Square();
+        Rectangle sq2 = new Square(5);
+        Rectangle sq3 = new Square(7, 3, 3);
+        Rectangle sq4 = new Square(new Square(8));
 
         System.out.println(sq1); // Rectangle{height=1, width=1, xPos=1, yPos=1}
         System.out.println();
@@ -17,5 +17,7 @@ public class SquareApp {
         System.out.println();
 
         System.out.println(sq4); // Rectangle{height=8, width=1, xPos=1, yPos=1}
+        System.out.println(Square.CORNERS); // prints 4
+        System.out.println(Square.getCount());// prints 5
     }
 }
