0da969e55b1f6fd0b2634c524bc005e36c8c425a
Mert <mertdemirok80@gmail.com>
2020-11-07 23:24:40 +0100

    implement unique equals method to all classes and test it

---
+++ Chapter10_Inheritance&Polymorphism/src/be/intecbrussel/exercise10_08/ShapeApp.java
@@ -0,0 +1,39 @@
+package be.intecbrussel.exercise10_08;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5, 3, 3);
+        Rectangle rectangle = new Rectangle(15, 20, 5, 5);
+        Square square = new Square(4, 6, 6);
+        Triangle triangle = new Triangle(10, 20, 5, 7, 7);
+        IsoScelesTriangle isoScelesTriangle = new IsoScelesTriangle(20, 20, 3, 3);
+
+        System.out.println(circle);
+        System.out.println(rectangle);
+        System.out.println(square);
+        System.out.println(triangle);
+        System.out.println(isoScelesTriangle);
+        System.out.println(Shape.getCount()); // prints 5
+        System.out.println();
+
+        Circle circle2 = new Circle(5, 3, 3);
+        Rectangle rectangle2 = new Rectangle(15, 20, 5, 5);
+        Square square2 = new Square(4, 6, 6);
+        Triangle triangle2 = new Triangle(10, 20, 5, 7, 7);
+        IsoScelesTriangle isoScelesTriangle2 = new IsoScelesTriangle(20, 20, 3, 3);
+
+        System.out.println();
+        System.out.println(circle.equals(circle2)); // true
+        System.out.println(rectangle.equals(rectangle2)); // true
+        System.out.println(square.equals(square2)); // true
+        System.out.println(rectangle.equals(rectangle)); // true
+        System.out.println(isoScelesTriangle.equals(isoScelesTriangle2)); // true
+
+        System.out.println();
+        circle2.setRadius(6);
+        System.out.println(circle.equals(circle2)); // false
+
+        Circle circle3 = new Circle();
+        System.out.println(circle.equals(circle3)); // false
+    }
+}
