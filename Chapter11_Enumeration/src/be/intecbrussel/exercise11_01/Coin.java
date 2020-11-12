bd8d293b39053b937dd5e29512a86d9b5f8cb7d3
Mert <mertdemirok80@gmail.com>
2020-11-12 19:59:54 +0100

    refactor exercise part02 CoinApp

--- Chapter11_Enumeration/src/be/intecbrussel/exercise11_01/Coin.java
+++ Chapter11_Enumeration/src/be/intecbrussel/exercise11_01/Coin.java
@@ -1,27 +1,33 @@
 package be.intecbrussel.exercise11_01;
 
 public enum Coin {
-    ONE_CENT(0.01),
-    TWO_CENTS(0.02),
-    FIVE_CENTS(0.05),
-    TEN_CENTS(0.10),
-    TWENTY_CENTS(0.20),
-    FIFTY_CENTS(0.50),
-    ONE_EURO(1),
-    TWO_EUROS(2);
+    ONE_CENT(1, "cent"),
+    TWO_CENTS(2, "cents"),
+    FIVE_CENTS(5, "cents"),
+    TEN_CENTS(10, "cents"),
+    TWENTY_CENTS(20, "cents"),
+    FIFTY_CENTS(50, "cents"),
+    ONE_EURO(1, "euro"),
+    TWO_EUROS(2, "euros");
 
-    private double value;
+    private int value;
+    private String type;
 
-    private Coin(double value){
+    private Coin(int value, String type){
         this.value = value;
+        this.type = type;
     }
 
     public double getValue() {
         return value;
     }
 
+    public String getType() {
+        return type;
+    }
+
     @Override
     public String toString() {
-        return name() +  "(" + value + ")";
+        return name() +  "(" + value + " " + type + ") ";
     }
 }
--- Chapter11_Enumeration/src/be/intecbrussel/exercise11_01/CoinApp.java
+++ Chapter11_Enumeration/src/be/intecbrussel/exercise11_01/CoinApp.java
@@ -1,19 +1,38 @@
 package be.intecbrussel.exercise11_01;
 
 public class CoinApp {
+
     public static void main(String[] args) {
         Coin[] coins = new Coin[8];
 
         for (int i = 0; i < Coin.values().length ; i++) {
-            coins[i] = Coin.values()[i];
+            int j = RandomGenerator.getNextInt(0, 8);
+            coins[i] = Coin.values()[j];
         }
+        calculateTotal(coins);
+    }
 
-        double total = 0;
+    public static void calculateTotal(Coin[] coins){
+        int totalCents = 0;
+        int totalEuros = 0;
+
         for (Coin c:coins
-             ) {
-            total+=c.getValue();
+        ) {
+            if(c.getType().contains("cent") || c.getType().contains("cents")){
+                totalCents+=c.getValue();
+                if(totalCents >= 100){
+                    totalEuros+=1;
+                    totalCents = totalCents - 100;
+                }
+            }
+            if(c.getType().contains("euro") || c.getType().contains("euros")){
+                totalEuros+=c.getValue();
+            }
         }
-        System.out.println("Total value: " + total);
+        printTotal(totalEuros, totalCents);
+    }
 
+    private static void printTotal(int euros, int cents){
+        System.out.println("Total value: " + euros + " euros" + " " + cents + " cents");
     }
 }
