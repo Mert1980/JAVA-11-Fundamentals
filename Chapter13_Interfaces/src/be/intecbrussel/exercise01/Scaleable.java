e4d18fc6d0116de65d018c3c34cbe2a879700121
Mert <mertdemirok80@gmail.com>
2020-11-15 14:45:18 +0100

    complete exercise01 interfaces, create an interface called Scaleable and compile it

---
+++ Chapter13_Interfaces/src/be/intecbrussel/exercise01/Scaleable.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.exercise01;
+
+public interface Scaleable {
+    // Constants
+    public static final int DOUBLE = 200;
+    public static final int HALF = 50;
+    public static final int QUARTER = 25;
+
+    // Methods
+    public abstract void scale(int factor);
+
+}
