ac1072a0a188f9aa356751fadb8f1620df85cc99
Mert <mertdemirok80@gmail.com>
2020-11-16 16:24:39 +0100

    Change MyPig Class to Pig Interface

--- Chapter13_Interfaces/src/be/intecbrussel/exercise03/PigApp.java
+++ Chapter13_Interfaces/src/be/intecbrussel/exercise03/PigApp.java
@@ -2,9 +2,10 @@ package be.intecbrussel.exercise03;
 
 public class PigApp {
     public static void main(String[] args) {
-        MyPig pig = new MyPig();
+        Pig pig = new MyPig();
 
         pig.grunt();
         pig.fly();
+
     }
 }
