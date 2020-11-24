70b196ef2e41358051714a2c43cf102702bb5699
Mert <mertdemirok80@gmail.com>
2020-11-24 18:13:30 +0100

    complete exercise02

---
+++ Chapter15_Exception_Handling/src/be/intecbrussel/exercise02/Division.java
@@ -0,0 +1,20 @@
+package be.intecbrussel.exercise02;
+
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        try {
+            Scanner keyboard = new Scanner(System.in);
+            int num = Integer.parseInt(keyboard.next());
+            int den = Integer.parseInt(keyboard.next());
+            int div = num / den;
+            System.out.printf("%d/%d=%d", num, den, div);
+            keyboard.close();
+        } catch (NumberFormatException nfe){
+            System.out.println("Invalid number");
+            System.out.println(nfe.getMessage());
+            nfe.printStackTrace();
+        }
+    }
+}
