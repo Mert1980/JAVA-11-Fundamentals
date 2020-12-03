6766285080be0fbc01dd01d29a8bef4e4964ed61
Mert <mertdemirok80@gmail.com>
2020-12-03 08:22:28 +0100

    create generic class ComparableDuo

---
+++ Chapter17_Generics/src/be/intecbrussel/exercise04/ComparableDuo.java
@@ -0,0 +1,22 @@
+package be.intecbrussel.exercise04;
+
+public class ComparableDuo<T extends Number & Comparable<T>> extends Duo<T> {
+    T first;
+    T second;
+
+    public ComparableDuo(T first, T second) {
+        super(first, second);
+    }
+
+    public T getHighest(){
+        return getFirst().compareTo(getSecond()) < 0 ? getSecond() : getFirst();
+    }
+
+    public T getLowest(){
+        return getFirst().compareTo(getSecond()) < 0 ? getFirst() : getSecond();
+    }
+
+    public int getSum(){
+        return getFirst().intValue() + getSecond().intValue();
+    }
+}
