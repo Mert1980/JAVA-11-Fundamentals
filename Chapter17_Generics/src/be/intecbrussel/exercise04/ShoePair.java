6b03696ca44e7655f44c7b595d12a7df244198ae
Mert <mertdemirok80@gmail.com>
2020-12-03 07:52:09 +0100

    add default constructor to ShoePair

--- Chapter17_Generics/src/be/intecbrussel/exercise04/ShoePair.java
+++ Chapter17_Generics/src/be/intecbrussel/exercise04/ShoePair.java
@@ -4,6 +4,9 @@ public class ShoePair<Shoe> implements Pair<Shoe>{
     private Shoe leftShoe;
     private Shoe rightShoe;
 
+    public ShoePair() {
+    }
+
     public ShoePair(Shoe leftShoe, Shoe rightShoe) {
         this.leftShoe = leftShoe;
         this.rightShoe = rightShoe;
