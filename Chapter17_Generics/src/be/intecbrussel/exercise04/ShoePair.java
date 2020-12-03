80cc6a558b073b087b7860f02efbf1934d163042
Mert <mertdemirok80@gmail.com>
2020-12-03 07:46:03 +0100

    create ShoePair class that implements the Pair<Shoe> Interface

---
+++ Chapter17_Generics/src/be/intecbrussel/exercise04/ShoePair.java
@@ -0,0 +1,31 @@
+package be.intecbrussel.exercise04;
+
+public class ShoePair<Shoe> implements Pair<Shoe>{
+    private Shoe leftShoe;
+    private Shoe rightShoe;
+
+    public ShoePair(Shoe leftShoe, Shoe rightShoe) {
+        this.leftShoe = leftShoe;
+        this.rightShoe = rightShoe;
+    }
+
+    @Override
+    public Shoe getLeft() {
+        return leftShoe;
+    }
+
+    @Override
+    public Shoe getRight() {
+        return rightShoe;
+    }
+
+    @Override
+    public void setLeft(Shoe left) {
+        this.leftShoe = left;
+    }
+
+    @Override
+    public void setRight(Shoe right) {
+        this.rightShoe = right;
+    }
+}
