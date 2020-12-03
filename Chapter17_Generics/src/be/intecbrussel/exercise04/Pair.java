c9de83e2a401e9f82c079696374acb5cbbd20086
Mert <mertdemirok80@gmail.com>
2020-12-03 07:36:38 +0100

    create generic Pair interface

---
+++ Chapter17_Generics/src/be/intecbrussel/exercise04/Pair.java
@@ -0,0 +1,15 @@
+package be.intecbrussel.exercise04;
+
+public interface Pair<E>{
+    public E getLeft();
+    public E getRight();
+
+    public void setLeft(E left);
+    public void setRight(E right);
+
+    public default void swap(){
+        E temp = getLeft();
+        setLeft(getRight());
+        setRight(temp);
+    }
+}
