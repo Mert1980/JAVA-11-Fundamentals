d28d01571a321240a2f98d4e5b361896ed4673f6
Mert <mertdemirok80@gmail.com>
2020-12-07 23:38:31 +0100

    replace the anonymous nested class with a lambda expression

--- Chapter18_Lambda_Expressions/src/be/intecbrussel/exercise01/TextApp.java
+++ Chapter18_Lambda_Expressions/src/be/intecbrussel/exercise01/TextApp.java
@@ -4,11 +4,6 @@ public class TextApp {
     public static void main(String[] args) {
         TextPrinter tp = new TextPrinter("Hello World");
         System.out.println("Words with letter 'e");
-        tp.filteredText(new WordFilter() {
-            @Override
-            public boolean isValid(String s) {
-                return s.contains("e");
-            }
-        });
+        tp.filteredText(s -> s.contains("e"));
     }
 }
