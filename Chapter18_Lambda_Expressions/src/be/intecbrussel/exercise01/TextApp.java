b10b20fdee57c66a9bab0258e9816094abcf8dd9
Mert <mertdemirok80@gmail.com>
2020-12-07 23:46:24 +0100

    complete exercise01 lambda expressions

--- Chapter18_Lambda_Expressions/src/be/intecbrussel/exercise01/TextApp.java
+++ Chapter18_Lambda_Expressions/src/be/intecbrussel/exercise01/TextApp.java
@@ -2,8 +2,28 @@ package be.intecbrussel.exercise01;
 
 public class TextApp {
     public static void main(String[] args) {
-        TextPrinter tp = new TextPrinter("Hello World");
+        TextPrinter tp = new TextPrinter("Hello to lambda expressions");
         System.out.println("Words with letter 'e");
         tp.filteredText(s -> s.contains("e"));
+
+        System.out.println("Words longer than 4");
+        tp.filteredText(s -> s.length() > 4);
+
+        System.out.println("Words beginning with 'a");
+        tp.filteredText(s -> s.charAt(0) == 'a');
+
+        System.out.println("Words that have 'e' as a second character");
+        tp.filteredText((s -> s.charAt(1) == 'e'));
+
+        System.out.println("Words that have 'e' 2 times");
+        tp.filteredText(s -> {
+            int count = 0;
+            for (char ch:s.toCharArray()) {
+                if(ch == 'e'){
+                    count++;
+                }
+            }
+            return count == 2;
+        });
     }
 }
