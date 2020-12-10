df8579a0ef7c88d8869e680f66f74168c6f579f6
Mert <mertdemirok80@gmail.com>
2020-12-10 23:18:24 +0100

    change name of the person

--- Chapter19_Streaming_API/src/be/intecbrussel/exercise04/StreamingApp.java
+++ Chapter19_Streaming_API/src/be/intecbrussel/exercise04/StreamingApp.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 public class StreamingApp {
     public static void main(String[] args) {
         Person person1 = new Person("Mert", "Demirok", Person.Gender.MALE, 40, 82, 182);
-        Person person2 = new Person("Ali", "Kaptan", Person.Gender.MALE, 34, 70, 169);
+        Person person2 = new Person("Ahmet", "Kaptan", Person.Gender.MALE, 34, 70, 169);
         Person person3 = new Person("Suzan", "Bekci", Person.Gender.FEMALE, 25, 60, 170);
         Person person4 = new Person("Almira", "Demirok", Person.Gender.FEMALE, 3, 17, 85);
 
@@ -129,6 +129,4 @@ public class StreamingApp {
         }
         return false;
     }
-
-
 }
