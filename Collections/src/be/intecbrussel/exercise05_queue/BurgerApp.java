16c9a32d5626b1a507fcae02af8a7cf34cd4cf19
Mert <mertdemirok80@gmail.com>
2020-12-14 21:16:28 +0100

    complete exercise05 use Class PriorityQueue

---
+++ Collections/src/be/intecbrussel/exercise05_queue/BurgerApp.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.exercise05_queue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class BurgerApp {
+    public static void main(String[] args) {
+
+        Queue<BurgerOrder> orders = new LinkedList<>();
+        orders.offer(new BurgerOrder("BigMacMenu", 2));
+        orders.offer(new BurgerOrder("ChickenMenu", 1));
+        orders.offer(new BurgerOrder("VegiMenu", 3));
+        orders.offer(new BurgerOrder("KidsMenu", 2));
+
+        BurgerOrder order = orders.peek();
+
+        while(order != null){
+            System.out.println("About to prepare " + order);
+            order = orders.poll();
+            System.out.println("Preparing " + order);
+            order = orders.peek();
+        }
+    }
+}
---
+++ Collections/src/be/intecbrussel/exercise05_queue/BurgerOrder.java
@@ -0,0 +1,32 @@
+package be.intecbrussel.exercise05_queue;
+
+public class BurgerOrder {
+    private String name;
+    private int number;
+
+    public BurgerOrder(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return  number + " " + name;
+    }
+}
