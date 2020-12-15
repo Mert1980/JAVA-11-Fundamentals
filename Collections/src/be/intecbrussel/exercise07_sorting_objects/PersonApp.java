9353df2810a6b203552c38ae7bf1f1bec20177ba
Mert <mertdemirok80@gmail.com>
2020-12-15 09:34:15 +0100

    sort the persons according to their weight. Create an anonymous Comparator Class and pass this as an argument to TreeSet constructor.

--- Collections/src/be/intecbrussel/exercise07_sorting_objects/PersonApp.java
+++ Collections/src/be/intecbrussel/exercise07_sorting_objects/PersonApp.java
@@ -1,15 +1,20 @@
 package be.intecbrussel.exercise07_sorting_objects;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Collection<Person> personSet = new TreeSet<>(new AgeComparator()); // causes ClassCastException if Comparable Interface is not
-        // implemented
+        // causes ClassCastException if Comparable Interface is not implemented
+        Collection<Person> personSet = new TreeSet<>(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return (int)o1.getWeight() - (int)o2.getWeight();
+            }
+        });
 
-        Person person1 = new Person("Mert", "Demirok", "male", 40, 82.4, 182);
+        Person person1 = new Person("Mert", "Demirok", "male", 40, 22.4, 182);
         Person person2 = new Person("Asena", "Bekci", "female", 39, 72.4, 165);
         Person person3 = new Person("Yigit", "Aslan", "male", 12, 40.4, 154);
         Person person4 = new Person("Yigit", "Aslan", "male", 12, 40.4, 154);
