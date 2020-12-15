36ded4e948b273478ae104934d55a680733b6de2
Mert <mertdemirok80@gmail.com>
2020-12-15 07:52:58 +0100

    sort the persons according to their last name. Implement Comparable Interface

---
+++ Collections/src/be/intecbrussel/exercise07_sorting_objects/Person.java
@@ -0,0 +1,95 @@
+package be.intecbrussel.exercise07_sorting_objects;
+
+import java.util.Objects;
+
+public class Person implements Comparable<Person>{
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+    private double weight;
+    private double height;
+
+    public Person(String firstName, String lastName, String gender, int age, double weight, double height) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Person person = (Person) o;
+        return age == person.age && Double.compare(person.weight, weight) == 0 && Double.compare(person.height,
+                height) == 0 && firstName.equals(person.firstName) && lastName.equals(person.lastName) && gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, gender, age, weight, height);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", gender='" + gender + '\'' + ", age=" + age + ", weight=" + weight + ", height=" + height + '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.getLastName().compareTo(o.getLastName());
+    }
+}
---
+++ Collections/src/be/intecbrussel/exercise07_sorting_objects/PersonApp.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.exercise07_sorting_objects;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Collection<Person> personSet = new TreeSet<>(); // causes ClassCastException if Comparable Interface is not
+        // implemented
+
+        Person person1 = new Person("Mert", "Demirok", "male", 40, 82.4, 182);
+        Person person2 = new Person("Asena", "Bekci", "female", 39, 72.4, 165);
+        Person person3 = new Person("Yigit", "Aslan", "male", 12, 40.4, 154);
+        Person person4 = new Person("Yigit", "Aslan", "male", 12, 40.4, 154);
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+        personSet.add(person4);
+
+        personSet.forEach(System.out::println); // order of the elements are different from the entry order
+    }
+}
