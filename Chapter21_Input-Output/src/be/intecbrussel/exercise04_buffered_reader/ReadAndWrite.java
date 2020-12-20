3aad5450fe1e61eb4f1836cb5cd87b183937e8a3
Mert <mertdemirok80@gmail.com>
2020-12-20 09:07:57 +0100

    use Class Files to make a new BufferedReader with a given path

--- Chapter21_Input-Output/src/be/intecbrussel/exercise04_buffered_reader/ReadAndWrite.java
+++ Chapter21_Input-Output/src/be/intecbrussel/exercise04_buffered_reader/ReadAndWrite.java
@@ -1,9 +1,13 @@
 package be.intecbrussel.exercise04_buffered_reader;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ReadAndWrite {
     public static void main(String[] args) {
+        Path path = Paths.get("File.txt");
         try(BufferedWriter writer = new BufferedWriter(new FileWriter("File.txt", true))){
             writer.write("This is first line");
             writer.newLine();
@@ -14,7 +18,7 @@ public class ReadAndWrite {
             System.out.println(ex.getMessage());
         }
         // read the file line by line
-        try(BufferedReader reader = new BufferedReader(new FileReader("File.txt"))) {
+        try(BufferedReader reader = Files.newBufferedReader(path)) {
             String line = null;
             while((line = reader.readLine()) != null){
                 System.out.println(line);
