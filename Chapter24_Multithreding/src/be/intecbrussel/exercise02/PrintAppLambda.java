41f313f7081943fb13730c44f34671ffc5d944a5
Mert <mertdemirok80@gmail.com>
2021-01-19 12:54:33 +0100

    get different thread states

--- Chapter24_Multithreding/src/be/intecbrussel/exercise02/PrintAppLambda.java
+++ Chapter24_Multithreding/src/be/intecbrussel/exercise02/PrintAppLambda.java
@@ -4,8 +4,12 @@ public class PrintAppLambda {
     public static void main(String[] args) {
         Thread thread1 = new Thread(()->print('c', 100));
         Thread thread2 = new Thread(()->print('*', 100));
+        System.out.println(thread1.getState());
+        System.out.println(thread2.getState());
         thread1.start();
+        System.out.println(thread1.getState());
         thread2.start();
+        System.out.println(thread2.getState());
         print('&', 100);
     }
 
