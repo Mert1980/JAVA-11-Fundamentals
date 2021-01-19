07293530251a603fa504d9fa9435aa211bbd7b11
Mert <mertdemirok80@gmail.com>
2021-01-19 10:12:35 +0100

    print two different set of chars using two different threads

---
+++ Chapter24_Multithreding/src/be/intecbrussel/exercise01/PrintApp.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.exercise01;
+
+public class PrintApp {
+    public static void main(String[] args) {
+        PrintThread thread1 = new PrintThread('*', 100);
+        PrintThread thread2 = new PrintThread('&', 100);
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ Chapter24_Multithreding/src/be/intecbrussel/exercise01/PrintThread.java
@@ -0,0 +1,18 @@
+package be.intecbrussel.exercise01;
+
+public class PrintThread extends Thread{
+    private char c;
+    private int count;
+
+    public PrintThread(char c, int count){
+        this.c = c;
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < count; i++) {
+            System.out.print(c);
+        }
+    }
+}
