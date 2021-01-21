d2dac2899c1a5ff7f1b2582a2f49e33eb88acfb6
Mert <mertdemirok80@gmail.com>
2021-01-21 05:50:19 +0100

    decrement counter with 2 different synchronized threads

--- Chapter24_Multithreding/src/be/intecbrussel/exercise07_object_locking/CounterAppLambda.java
+++ Chapter24_Multithreding/src/be/intecbrussel/exercise07_object_locking/CounterAppLambda.java
@@ -5,24 +5,32 @@ public class CounterAppLambda {
         Counter counter = new Counter();
         Thread thread1 = new Thread(()-> increment(counter, 10000));
         Thread thread2 = new Thread(()-> increment(counter, 10000));
-
+        Thread thread3 = new Thread(()-> decrement(counter, 5000));
+        Thread thread4 = new Thread(()-> decrement(counter, 5000));
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
 
         try {
             thread1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
             thread2.join();
+            thread3.join();
+            thread4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         System.out.println(counter.getCount()); // 12231 before synchronized
                                                 // 20000 after synchronized
+                                                // 10000 after calling decrement method
 
+    }
+
+    private static void decrement(Counter counter, int number) {
+        for (int i = 0; i < number; i++) {
+            counter.decrement();
+        }
     }
 
 
